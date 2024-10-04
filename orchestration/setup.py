d204f922b343496a8937a79f5c63223d336b74c9
danieloselu3 <doselu3@gmail.com>
2024-10-04 21:04:08 +0300

    initial dbt commit

--- orchestration/setup.py
+++ orchestration/setup.py
@@ -5,7 +5,20 @@ setup(
     packages=find_packages(exclude=["orchestration_tests"]),
     install_requires=[
         "dagster",
-        "dagster-cloud"
+        "dagster-cloud",
+        "dagster-postgres",
+        "dagster-dbt",
+        "dbt-postgres",
+        "geopandas",
+        "kaleido",
+        "pandas[parquet]",
+        "plotly",
+        "shapely",
+        "smart_open[s3]",
+        "s3fs",
+        "smart_open",
+        "boto3",
+        "pyarrow",
     ],
     extras_require={"dev": ["dagster-webserver", "pytest"]},
 )
